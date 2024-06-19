07a7379eb3eb1270ead0b4dd6d128fe0a7fabf55
Keenan Faulkner <keenan.r.faulkner@gmail.com>
2024-06-18 17:23:39 -0700

    updated about and skills

--- src/components/About.js
+++ src/components/About.js
@@ -9,27 +9,24 @@ const About = () => {
                 <div className='min-w-[300px] max-w-[1000px] flex flex-col justify-center'>
                     <h2 className="text-[#c1c9d9] font-medium text-[18pt]">About Me</h2>
                     <p className="text-[#c1c9d9] text-[12pt] mt-[20px] xl:pr-[100px] pb-[60px]">
-                        I began learning how to write code in 2018. 
-                        Since then I've served a mission for The Church of Jesus Christ of Latter-Day Saints, for two years, 
-                        in both Sydney Australia and Las Vegas Nevada, and I now attend BYU in Provo, UT. I'm a computer engineering 
-                        major, scheduled to graduate in the spring of 2025. I love to learn, and it's one of the things that adds to 
-                        my abilities as a developer. In high school, I finished all my general education classes and graduated with 
-                        an Associate's Degree. I was named the Provo High School Mathematics Sterling Scholar, graduated with a 4.0 GPA, 
-                        was the salutatorian, and earned a 35/36 on the ACT.
+                        I'm an experienced computer engineer, ready to fill needs and elevate the performance of your team.
+                        I have experience refining system architectures using parallel processing and templating, writing low-level programs
+                        for embedded applications, enhancing GUI based applications in languages including Python, C++, and Java, and 
+                        doing integration/physical layout work for microprocessor design.
                         <br />
                         <br />
+                        I'm a trusted and proven developer. Most recently, I was selected to be part of the Full-Chip Layout team at Intel
+                        Corporation due to my experience with scripting languages; the cohort had approximately 50 interns and I was one of only
+                        2 selected for FCL. In other roles, I've been put in charge of large projects, such as one to rewrite my team's codebase
+                        to incorporate parallel performance. Additionally, I've published academic papers on new methodologies for identifying IP
+                        in unknown FPGA bitstreams, in IEEE journals.
+                        <br/>
+                        <br/>
                         I have many other hobbies outside of software development. I enjoy being active by hiking with my wife, Nikki, and 
                         playing basketball. I love listening to music, especially by the Las Vegas band The Killers, and I 
-                        often make covers of other artist's songs at home with my piano. In fact, my first major web-programming 
-                        project was focused on my love of The Killers and my own ability to make music.
-                        <br/>
-                        <br/>
-                        I want to be the best programmer I can. Although I believe that a good work-life balance is important, 
-                        I often get caught up for hours working on projects, researching technology, and watching videos about development in 
-                        my free time. I believe in going above and beyond. For example, with my Killers Project, which was a capstone for a 
-                        DevMountain bootcamp I was involved in, I taught myself React, React Router, and TailwindCSS. 
-                        I put in well over 150 hours of my own free time on that project over the course of five weeks, 
-                        even though the capstone requirements expected only html/vanilla JS & CSS, and 3 working features.
+                        often make covers of other artist's songs in my home studio. In fact, my first major web-programming 
+                        project was focused on my love of The Killers and my own ability to make music. Welcome to my site! I look forward to
+                        meeting you and exploring a future together.
                     </p>
                 </div>
                 <div className='xl:border-l-[1px] xl:border-t-0 border-t-[1px] sm:px-[60px] pborder-[#808691] xl:pl-[100px]' >
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,7 +15,8 @@ import {
     SiReactrouter as ReactRouterIcon,
     SiGit as GitIcon,
     SiPython as PythonIcon,
-    SiSpring as SpringIcon
+    SiSpring as SpringIcon,
+    SiGithub as GithubIcon,
 } from 'react-icons/si';
 import { VscTerminalLinux as LinuxIcon  } from "react-icons/vsc";
 import useDocumentTitle from '../hooks/useDocumentTitle';
@@ -28,21 +29,22 @@ const Skills = () => {
                 <SkillIcon icon={<PythonIcon size='80' className='text-[#c414c4] group-hover:text-[#ff00ff]' />} tooltip='Python'/>
                 <SkillIcon icon={<CppIcon size='80' className='text-[#233f70] group-hover:text-[#3c64a7]' />} tooltip='C++'/>
                 <SkillIcon icon={<GitIcon size='80' className='text-[#e44c30] group-hover:text-[#fb6f56]' />} tooltip='Git' />
+                <SkillIcon icon={<GithubIcon size='80' className='text-[#b9a9a7] group-hover:text-[#ffffff]' />} tooltip='GitHub' />
                 <SkillIcon icon={<LinuxIcon size='80' className='text-[#ba8d07] group-hover:text-[#ffbf00]' />} tooltip='Linux' />
-                <SkillIcon icon={<BashIcon size='80' className='text-[#50b63c] group-hover:text-[#76da70]' />} tooltip='Bash'/>
+                <SkillIcon icon={<BashIcon size='80' className='text-[#50b63c] group-hover:text-[#76da70]' />} tooltip='Scripting'/>
                 <SkillIcon icon={<RTLDesignIcon size='80' className='text-[#3d976d] group-hover:text-[#6ddd83]' />} tooltip='RTL Design' />
-                <SkillIcon icon={<RoutingIcon size='80' className='text-[#a9903d] group-hover:text-[#cfbc7e]' />} tooltip='Analog Routing (Fusion Compiler)'/>
+                <SkillIcon icon={<RoutingIcon size='80' className='text-[#a9903d] group-hover:text-[#cfbc7e]' />} tooltip='Analog Routing (Synopsys Fusion Compiler)'/>
                 <SkillIcon icon={<JavaIcon size='80' className='text-[#d0350d] group-hover:text-[#ff3300]' />} tooltip='Java'/>
+                <SkillIcon icon={<ReactIcon size='80' className='text-[#4fb7d3] group-hover:text-[#83e4ff]' />} tooltip='ReactJS'/>
                 <SkillIcon icon={<HTMLIcon size='80' className='text-[#d74b24] group-hover:text-[#ff6034]' />} tooltip='HTML'/>
                 <SkillIcon icon={<CSSIcon size='80' className='text-[#2091eb] group-hover:text-[#4eafff]' />} tooltip='CSS'/>
                 <SkillIcon icon={<JSIcon size='80' className='text-[#e4d04b] group-hover:text-[#ebff39]' />} tooltip='JavaScript'/>
                 <SkillIcon icon={<NodeIcon size='80' className='text-[#5aa844] group-hover:text-[#7ce65f]' />} tooltip='NodeJS'/>
+                <SkillIcon icon={<SpringIcon size='80' className='text-[#568c31] group-hover:text-[#6aad3d]' />} tooltip='Spring Framework' />
                 <SkillIcon icon={<ExpressIcon size='80' className='text-[#808691] group-hover:text-white' />} tooltip='ExpressJS'/>
                 <SkillIcon icon={<PostgresIcon size='80' className='text-[#30628a] group-hover:text-[#458ac2]' />} tooltip='SQL/Postgres'/>
-                <SkillIcon icon={<ReactIcon size='80' className='text-[#4fb7d3] group-hover:text-[#83e4ff]' />} tooltip='ReactJS'/>
                 <SkillIcon icon={<ReactRouterIcon size='80' className='text-[#c5021a] group-hover:text-[#e42138]' />} tooltip='React Router'/>
                 <SkillIcon icon={<TailwindIcon size='80' className='text-[#07adca] group-hover:text-[#53ccff]' />} tooltip='Tailwind CSS'/>
-                <SkillIcon icon={<SpringIcon size='80' className='text-[#568c31] group-hover:text-[#6aad3d]' />} tooltip='Spring Framework' />
             </div>
         </div>
     )
